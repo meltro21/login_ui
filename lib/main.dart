3c50822db93af1997b36a86164ccfa9c81e7234f
meltro <meltro212121@gmail.com>
2022-08-12 23:35:29 +0500

    shapes done 	created all the background shapes.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -35,11 +37,119 @@ class _MyHomePageState extends State<MyHomePage> {
     var mediaHeight = MediaQuery.of(context).size.height;
     var mediaWidth = MediaQuery.of(context).size.width;
     var statusBarHeight = MediaQuery.of(context).padding.top;
+    mediaHeight = mediaHeight - statusBarHeight;
+    print('height is $mediaHeight');
     return Scaffold(
       body: SafeArea(
-          child: Container(
-        child: Stack(children: []),
-      )),
+        child: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color(0xff5D54A4), Color(0xff7C78B8)])),
+          height: mediaHeight,
+          width: mediaWidth,
+          child: Stack(children: [
+            //Shape 1
+            Positioned(
+              top: mediaHeight * 0.7,
+              right: mediaWidth * 0.1,
+              child: Transform.rotate(
+                angle: 0.785398,
+                child: Container(
+                  height: mediaHeight * 0.6,
+                  width: mediaWidth * 0.6,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(60),
+                    color: const Color(0xff7E7BB9),
+                  ),
+                ),
+              ),
+            ),
+            //Shape 2
+            Positioned(
+              top: mediaHeight * 0.1,
+              right: 0,
+              child: Transform.rotate(
+                angle: 0.785398,
+                child: Container(
+                  height: mediaHeight * 0.7,
+                  width: mediaWidth * 0.6,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(32),
+                    gradient: LinearGradient(
+                        transform: GradientRotation(4.71239),
+                        colors: [Color(0xff5D54A4), Color(0xff6A679E)]),
+                  ),
+                ),
+              ),
+            ),
+            //Shape 3
+            Positioned(
+              top: -mediaHeight * 0.17,
+              right: 0,
+              child: Transform.rotate(
+                angle: 0.785398,
+                child: Container(
+                  height: mediaHeight * 0.2,
+                  width: mediaWidth * 0.8,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(32),
+                    color: Color(0xff6C63AC),
+                  ),
+                ),
+              ),
+            ),
+            //Shape 4
+            Positioned(
+              top: mediaHeight * 0.07,
+              // left: -mediaWidth * 0.8,
+
+              right: mediaWidth * 0.6,
+              child: Transform.rotate(
+                angle: 0.785398,
+                child: Container(
+                  height: mediaHeight,
+                  width: mediaWidth * 1.25,
+                  decoration: BoxDecoration(
+                    borderRadius:
+                        BorderRadius.only(topRight: Radius.circular(72)),
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            ),
+            // Positioned(
+            //   top: (mediaHeight / 2),
+            //   left: mediaWidth / 2,
+            //   child: Transform.rotate(
+            //     angle: 0.78,
+            //     child: Container(
+            //       height: 300,
+            //       width: 10,
+            //       color: Colors.teal,
+            //     ),
+            //   ),
+            // ),
+            // Positioned(
+            //   top: (mediaHeight / 2),
+            //   left: mediaWidth / 2,
+            //   child: Container(
+            //     height: 10,
+            //     width: 10,
+            //     color: Colors.red,
+            //   ),
+            // ),
+            // Positioned(
+            //   top: (mediaHeight / 2) + 150,
+            //   left: mediaWidth / 2,
+            //   child: Container(
+            //     height: 10,
+            //     width: 10,
+            //     color: Colors.orange,
+            //   ),
+            // ),
+          ]),
+        ),
+      ),
     );
   }
 }
